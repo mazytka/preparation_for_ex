fdee7c7011ca0a213fb35bdf11beefa6b0536e43
mazytka <95392543+mazytka@users.noreply.github.com>
2023-05-24 06:44:21 +0300

    add MainMenu.py and func about

---
+++ MainMenu.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QMenuBar
+
+
+class MainMenu(QMenuBar):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        help_menu = self.addMenu('Help')
+
+        self.__about = help_menu.addAction('About the program...')
+        self.about_qt = help_menu.addAction('about Qt...')
+
+
+    @property
+    def about(self):
+        return self.__about
+
+
--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,26 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.QtCore import pyqtSlot
+from MainMenu import MainMenu
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.title = 'Managing tasks for students'
+
+        main_menu = MainMenu(parent=self)
+        self.setMenuBar(main_menu)
+
+        main_menu.about_qt.triggered.connect(self.about_qt)
+        main_menu.about.triggered.connect(self.about)
+
+    @pyqtSlot()
+    def about(self):
+        text = 'The program is designed to manage tasks and assignments for students'
+        QMessageBox.about(self, self.title, text)
+
+    @pyqtSlot()
+    def about_qt(self):
+        QMessageBox.aboutQt(self, self.title)
